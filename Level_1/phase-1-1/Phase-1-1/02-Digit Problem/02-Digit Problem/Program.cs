1e7f8668376617f0d3efbcbbe44db5cfcb292271
Sameh Serag <36167068+SamehSerag@users.noreply.github.com>
2023-12-06 00:30:22 +0200

    first commet

---
+++ Level_1/phase-1-1/Phase-1-1/02-Digit Problem/02-Digit Problem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace _02_Digit_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder X = new StringBuilder (input.Split(' ')[0]);
+            short K = short.Parse(input.Split(' ')[1]);
+
+            short changes = 0;
+            for (int i = 0; i < X?.Length && changes < K ; i++)
+            {
+                if(X[i] != '9')
+                {
+                    X[i] = '9';
+                    changes++;
+                }
+
+            }
+
+            Console.WriteLine(X);
+        }
+    }
+}
