04d5f1f06f90da0d242c02578efbb438d44712d4
Sameh Serag <36167068+SamehSerag@users.noreply.github.com>
2023-12-06 02:15:35 +0200

    Add 05-Rain answer

---
+++ Level_1/Phase-1-1/03-Fitting Circle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03_Fitting_Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int testCases = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < testCases; i++)
+            {
+                var splitedInput = Console.ReadLine().Split(' ');
+                int length = int.Parse(splitedInput[0]);
+                int width = int.Parse(splitedInput[1]);
+
+                Console.WriteLine((length/width) == 0 ? width/length : (length / width));
+            }
+           
+
+
+        }
+    }
+}
---
+++ Level_1/Phase-1-1/05-Rain/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace _05_Rain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < testCases; i++)
+            {
+                var splitedInput = Console.ReadLine().Split(' ');
+                int rangeStart = int.Parse(splitedInput[0]);
+                int rangeEnd = int.Parse(splitedInput[1]);
+                int soundsByClouds = int.Parse(splitedInput[2]);
+                
+                int minClouds = (int)Math.Ceiling(Convert.ToDouble(rangeStart) / soundsByClouds);
+                int maxClouds = (int)Math.Floor(Convert.ToDouble(rangeEnd) / soundsByClouds);
+
+                if(minClouds > maxClouds)
+                {
+                    minClouds = -1;
+                    maxClouds = -1;
+                }
+
+                Console.WriteLine($"{minClouds} {maxClouds}");
+            }
+        }
+    }
+}
