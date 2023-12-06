6ab8264c9ce2013d4c80e8a24db1ce9acc644073
Sameh Serag <36167068+SamehSerag@users.noreply.github.com>
2023-12-07 01:23:14 +0200

    Add 08-Distribute Choclate

---
+++ Level_1/Phase-1-1/08-Distribute Chocolates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _08_Distribute_Chocolates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < testCases; i++)
+            {
+                var splitedInput = Console.ReadLine().Split(' ');
+                long chocolates = long.Parse(splitedInput[0]);
+                int studentsNumber = int.Parse(splitedInput[1]);
+
+                long totalDistributedChocolates = ((long)studentsNumber * (studentsNumber + 1)) / 2;
+
+                if(totalDistributedChocolates > chocolates)
+                {
+                    Console.WriteLine(chocolates);
+                    continue;
+                }
+
+                long remain = (chocolates - totalDistributedChocolates) % studentsNumber;
+                Console.WriteLine(remain);
+
+               
+            }
+        }
+    }
+}
