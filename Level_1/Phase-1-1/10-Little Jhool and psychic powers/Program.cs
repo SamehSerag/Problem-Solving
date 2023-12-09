5468406820682179c85c0c1ab6c0b0a33a7f2317
Sameh Serag <36167068+SamehSerag@users.noreply.github.com>
2023-12-09 19:56:18 +0200

    Add 10-Little Jhool and psychic powers

---
+++ Level_1/Phase-1-1/10-Little Jhool and psychic powers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _10_Little_Jhool_and_psychic_powers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(BadOrGoodLife(Console.ReadLine()));
+        }
+
+        static string BadOrGoodLife(string input)
+        {
+            var arrInput = input.ToCharArray();
+            const int windowLength = 6;
+            const int zeroAscii = 48;
+            int sequenceSum = 0;
+
+            if(arrInput?.Length < 6)
+                return "Good luck!";
+
+            for (int i = 0; i < windowLength; i++)
+                sequenceSum += Convert.ToInt16(arrInput[i]) - zeroAscii; //
+
+            if (sequenceSum == 0 || sequenceSum == 6)
+                return "Sorry, sorry!";            
+
+            for (int i = windowLength; i < arrInput?.Length; i++)
+            {
+                
+                sequenceSum += arrInput[i] - arrInput[i - windowLength];
+                if ((sequenceSum == 0 || sequenceSum == 6))
+                    return "Sorry, sorry!";
+
+            }
+
+            return "Good luck!";
+        }
+
+    }
+}
