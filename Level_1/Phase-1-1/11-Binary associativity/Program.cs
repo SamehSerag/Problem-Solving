2da8847bcca17e49c5d1ca1ae3e8e1ca156a2ee5
Sameh Serag <36167068+SamehSerag@users.noreply.github.com>
2023-12-10 19:37:11 +0200

    Added 12-

--- Level_1/Phase-1-1/11-Binary associativity/Program.cs
+++ Level_1/Phase-1-1/11-Binary associativity/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace _11_Binary_associativity
+﻿using System;
+using System.Linq;
+
+namespace _11_Binary_associativity
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int testCases = int.Parse(Console.ReadLine());
+            string[] a = new string[]
+            {
+                "0 1 1 0", "0 1 1 1", "0 0 0 1", "1 1 1 1", "0 0 1 1", "0 0 0 0", "0 1 0 1", "1 0 0 1"
+            };
+
+            for (int i = 0; i < testCases; i++)
+            {
+                string input =Console.ReadLine();
+
+                if (a.Any(x => x == input))
+                    Console.WriteLine("Yes");
+                else Console.WriteLine("No");
+
+            }
         }
     }
 }
---
+++ Level_1/Phase-1-1/12-HAWKEYE AND FLOODFILL/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _12_HAWKEYE_AND_FLOODFILL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            var input = Array.ConvertAll(Console.ReadLine().Split(' '), Convert.ToInt32);         
+            (int targetI, int targetJ, int power) = (input[0], input[1], input[2]);
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    int val = power - Math.Max(Math.Abs(i - targetI), Math.Abs(j - targetJ));
+                    if(val < 0)
+                        Console.Write("0 ");
+                    else
+                        Console.Write($"{val} ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+    }
+}
