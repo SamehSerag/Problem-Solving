82a4fd70f2cc2e92606db0172668ad0f3941fa01
Sameh Serag <36167068+SamehSerag@users.noreply.github.com>
2023-12-12 00:28:35 +0200

    Added Answer of 13-

--- Level_1/Phase-1-1/13-The Dice/Program.cs
+++ Level_1/Phase-1-1/13-The Dice/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace _13_The_Dice
+﻿using System;
+using System.Linq;
+
+namespace _13_The_Dice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //var dice = Array.ConvertAll(Console.ReadLine().ToCharArray(), Convert.ToInt32);
+            //var dice = Array.ConvertAll(Console.ReadLine().Split(""), Convert.ToInt32);
+
+            var dice = Console.ReadLine().Select(x => x - '0').ToArray();
+
+            Console.WriteLine(CountGirls(dice));
+            
+        }
+
+        public static int CountGirls(int[] dice)
+        {
+            int girles = 0;
+
+            if (dice.Any(x => x > 6) || dice[dice.Length - 1] == 6)
+            {
+                return -1;
+            }
+            for (int i = 0; i < dice?.Length; i++)
+            {
+                if (dice[i] != 6)
+                    girles++;
+            }
+            return girles;
         }
     }
 }
