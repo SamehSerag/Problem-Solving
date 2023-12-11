9ff456d6eeaa986607fc33f529f75fb0d57dad45
Sameh Serag <36167068+SamehSerag@users.noreply.github.com>
2023-12-11 23:42:58 +0200

    Added 13-

---
+++ Level_1/Phase-1-1/13-The Dice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _13_The_Dice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
