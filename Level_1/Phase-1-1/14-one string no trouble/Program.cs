5978612100e26d510d9ba28a0e094f713f539c5c
Sameh Serag <36167068+SamehSerag@users.noreply.github.com>
2023-12-12 22:01:29 +0200

    Added Answer of 14-

---
+++ Level_1/Phase-1-1/14-one string no trouble/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _14_one_string_no_trouble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            Console.WriteLine(LongistGoodString(input));
+
+        }
+
+        static long LongistGoodString(string input)
+        {
+           
+            int maxLength = 1;
+            int length = 1;
+           
+            for (int i = 0; i < input.Length-1; i++)
+            {
+                if (input[i] != input[i + 1])
+                    length++;
+                else
+                {
+                    maxLength = Math.Max(maxLength, length);
+                    length = 1;
+                }
+            }
+
+            maxLength = Math.Max(maxLength, length);
+
+            return maxLength;
+        }
+    }
+}
