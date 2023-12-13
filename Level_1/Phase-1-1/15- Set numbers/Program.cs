ad4abf780dd12bde9e18074514f11ab25ed42613
Sameh Serag <36167068+SamehSerag@users.noreply.github.com>
2023-12-13 22:22:47 +0200

    Added Answer of 15-

---
+++ Level_1/Phase-1-1/15- Set numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace _15__Set_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < testCases; i++)
+            {
+                string k = Console.ReadLine();
+                StringBuilder kBinary = new StringBuilder("");
+                int kNum = int.Parse(k);
+                long maxNumberWithSetBits = 0;
+
+                while(kNum != 0)
+                {
+                    if(kNum % 2 == 0)
+                        kBinary.Insert(0,"0");
+                    else
+                        kBinary.Insert(0,"1");
+                    kNum/=2;
+                }
+                kNum = int.Parse(k);
+                for (int j = 0; j < kBinary?.Length; j++)
+                {
+                    if (maxNumberWithSetBits + Math.Pow(2, j) <= kNum)
+                        maxNumberWithSetBits += (long)Math.Pow(2, j);
+                    else
+                        break;
+                }
+                Console.WriteLine(maxNumberWithSetBits);
+            }
+
+        }
+    }
+}
